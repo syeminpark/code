9cc584bb86662b2d4397bae96b5a9fe3378a4061
syeminpark <70131115+syeminpark@users.noreply.github.com>
2021-03-02 23:31:49 +0900

    finish c start javascript

---
+++ .vscode/javascript/chp4/chp4.js
@@ -0,0 +1,12 @@
+//chapter 4 콜백 함수
+
+/*
+ 콜백 함수는 다른 코드의 인자로 넘겨주는 함수
+ let timer=setInterval(cbFunc,300);
+ timer 변수에는 setInterval의 ID가 담긴다. 반복을 중단하기 위해 필요
+
+ map메서드
+ 첫번째 인자로 callback, 생략 가능한 두번째 인자로 콜백함 수 내부에서 this로 인식할 대상을 특정
+ 제이쿼리
+ 
+*/
